095584ba5887841dac59822d8cc9d1f639c3d1db
Francis Wang <go717franciswang@hotmail.com>
2014-05-21 01:41:15 -0700

    Solve issue #1

--- README.md
+++ README.md
@@ -1,20 +1,14 @@
 ### Dependencies
 * google/api_client
-* sinatra
 * inifile
 
 ### Setup
-1. go to [Google API console] and set up "Client ID for native application"
-    * "Client ID for web application" appears to forget the credential after a while
-1. copy and paste "Client ID" and "Client secret" to config.ini
-1. set "Redirect URIs" to http://localhost:4567/oauth2callback, which is the callback page OAuth redirects to. This page is temporarily hosted on Sinatra. It is used to capture the access_token, which is stored in drive_token.yaml
-1. start Sinatra server, ```ruby authorize.rb```
-1. visit http://localhost:4567
-1. check if drive_token.yaml is generated and has access token
+1. Go to [Google API console] and set up "Client ID for native application"
+1. "Download JSON" to project directory, and rename to client_secrets.json
 
 ### Use
 * set folder_id, where you want backup/*.db synced to
-* run ```sync.rb``` to sync to google drive once
+* Run ```ruby sync.rb``` for the first time to manually authenticate
 * set up crontab at appropriate times to sync regularly
 
 ### Development
--- sync.rb
+++ sync.rb
@@ -1,112 +1,174 @@
+# Copyright (C) 2012 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 require 'google/api_client'
+require 'google/api_client/client_secrets'
+require 'google/api_client/auth/file_storage'
+require 'google/api_client/auth/installed_app'
+require 'logger'
 require 'inifile'
-require 'json'
-require 'yaml'
-require 'digest'
-require 'set'
 
-curdir = File.dirname(__FILE__)
-config = IniFile.load(File.join(curdir, 'config.ini'))
+API_VERSION = 'v2'
+CACHED_API_FILE = "drive-#{API_VERSION}.cache"
+CREDENTIAL_STORE_FILE = "#{$0}-oauth2.json"
 
-client = Google::APIClient.new({'application_name' => 'drive', 'application_version' => '0.01'})
-client.authorization.client_id = config['main']['client_id']
-client.authorization.client_secret = config['main']['client_secret']
-client.authorization.scope = 'https://www.googleapis.com/auth/drive.file'
-drive = client.discovered_api('drive', 'v2')
+# Handles authentication and loading of the API.
+def setup()
+  log_file = File.open('drive.log', 'a+')
+  log_file.sync = true
+  logger = Logger.new(log_file)
+  logger.level = Logger::DEBUG
 
-access_token = File.join(curdir, 'drive_token.yaml')
-session = YAML.load_file(access_token)
-client.authorization.update_token!(session)
+  client = Google::APIClient.new(:application_name => 'Ruby Drive sample',
+      :application_version => '1.0.0')
 
-result = client.execute(
-  api_method: drive.children.list,
-  parameters: {
-    folderId: config['main']['google_drive_folder_id']
-  },
-  authorization: client.authorization
-)
+  # FileStorage stores auth credentials in a file, so they survive multiple runs
+  # of the application. This avoids prompting the user for authorization every
+  # time the access token expires, by remembering the refresh token.
+  # Note: FileStorage is not suitable for multi-user applications.
+  file_storage = Google::APIClient::FileStorage.new(CREDENTIAL_STORE_FILE)
+  if file_storage.authorization.nil?
+    client_secrets = Google::APIClient::ClientSecrets.load
+    # The InstalledAppFlow is a helper class to handle the OAuth 2.0 installed
+    # application flow, which ties in with FileStorage to store credentials
+    # between runs.
+    flow = Google::APIClient::InstalledAppFlow.new(
+      :client_id => client_secrets.client_id,
+      :client_secret => client_secrets.client_secret,
+      :scope => ['https://www.googleapis.com/auth/drive']
+    )
+    client.authorization = flow.authorize(file_storage)
+  else
+    client.authorization = file_storage.authorization
+  end
 
-gdrive_file_info = {}
-rs = JSON.load(result.body)
-rs['items'].each do |item|
-  id = item['id']
-  result = client.execute(
-    api_method: drive.files.get,
-    parameters: {
-      fileId: id
-    },
-    authorization: client.authorization
-  )
-  info = JSON.load(result.body)
-  title = info['title']
-  gdrive_file_info[title] = info
-end
-
-# if google drive contain the file and md5 matches, no change
-# if google drive contain the file and md5 does not match, update
-# if google drive does not contain the file, upload
-
-local_files = Set.new
-Dir.glob(File.join(config['main']['local_backup_dir'], '*.db')).each do |filepath|
-  filename = File.basename(filepath)
-  local_files.add(filename)
-  if gdrive_file_info.has_key?(filename)
-    md5 = Digest::MD5.file(filepath).hexdigest
-    if md5 != gdrive_file_info[filename]['md5Checksum']
-      puts "#{filename} md5 does not match, will update"
-      # https://developers.google.com/drive/v2/reference/files/update
-      mimetype = 'application/octet-stream'
-      file = drive.files.update.request_schema.new({
-        'title' => filename,
-        'description' => 'mnemosyne backup',
-        'mimeType' => mimetype,
-        'parents' => [{'id' => config['main']['google_drive_folder_id']}]
-      })
-
-      media = Google::APIClient::UploadIO.new(filepath, mimetype)
-      result = client.execute(
-        :api_method => drive.files.update,
-        :body_object => file,
-        :media => media,
-        :parameters => {
-          'uploadType' => 'multipart',
-          'alt' => 'json'
-        },
-        authorization: client.authorization
-      )
-    else
-      puts "#{filename} md5 matches, no need to update"
+  drive = nil
+  # Load cached discovered API, if it exists. This prevents retrieving the
+  # discovery document on every run, saving a round-trip to API servers.
+  if File.exists? CACHED_API_FILE
+    File.open(CACHED_API_FILE) do |file|
+      drive = Marshal.load(file)
     end
   else
-    puts "#{filename} does not exist on google drive, will upload"
-    # https://developers.google.com/drive/web/quickstart/quickstart-ruby#step_4_run_the_sample
-    # https://developers.google.com/drive/v2/reference/files/insert
-    mimetype = 'application/octet-stream'
-    file = drive.files.insert.request_schema.new({
-      'title' => filename,
-      'description' => 'mnemosyne backup',
-      'mimeType' => mimetype,
-      'parents' => [{'id' => config['main']['google_drive_folder_id']}]
-    })
-
-    media = Google::APIClient::UploadIO.new(filepath, mimetype)
-    result = client.execute(
-      :api_method => drive.files.insert,
-      :body_object => file,
-      :media => media,
-      :parameters => {
-        'uploadType' => 'multipart',
-        'alt' => 'json'
-      },
-      authorization: client.authorization
-    )
+    drive = client.discovered_api('drive', API_VERSION)
+    File.open(CACHED_API_FILE, 'w') do |file|
+      Marshal.dump(drive, file)
+    end
   end
+
+  return client, drive
 end
 
-session[:access_token] = client.authorization.access_token
-session[:refresh_token] = client.authorization.refresh_token
-session[:expires_in] = client.authorization.expires_in
-session[:issued_at] = client.authorization.issued_at
-File.open(access_token, 'w') do |out|
-  YAML.dump(session, out)
+# Handles files.insert call to Drive API.
+def get_grive_file_info(client, drive, config)
+  gdrive_file_info = {}
+
+  result = client.execute(
+    api_method: drive.children.list,
+    parameters: {
+      folderId: config['main']['google_drive_folder_id']
+    }
+  )
+
+  rs = JSON.load(result.body)
+  rs['items'].each do |item|
+    id = item['id']
+    result = client.execute(
+      api_method: drive.files.get,
+      parameters: {
+        fileId: id
+      }
+    )
+    info = JSON.load(result.body)
+    title = info['title']
+    gdrive_file_info[title] = info
+  end
+
+  gdrive_file_info
 end
+
+def update_file(client, drive, filepath, config)
+  # https://developers.google.com/drive/v2/reference/files/update
+  filename = File.basename(filepath)
+  mimetype = 'application/octet-stream'
+  file = drive.files.update.request_schema.new({
+    'title' => filename,
+    'description' => 'mnemosyne backup',
+    'mimeType' => mimetype,
+    'parents' => [{'id' => config['main']['google_drive_folder_id']}]
+  })
+
+  media = Google::APIClient::UploadIO.new(filepath, mimetype)
+  result = client.execute(
+    :api_method => drive.files.update,
+    :body_object => file,
+    :media => media,
+    :parameters => {
+      'uploadType' => 'multipart',
+      'alt' => 'json'
+    }
+  )
+end
+
+def insert_file(client, drive, filepath, config)
+  # https://developers.google.com/drive/web/quickstart/quickstart-ruby#step_4_run_the_sample
+  # https://developers.google.com/drive/v2/reference/files/insert
+  filename = File.basename(filepath)
+  mimetype = 'application/octet-stream'
+  file = drive.files.insert.request_schema.new({
+    'title' => filename,
+    'description' => 'mnemosyne backup',
+    'mimeType' => mimetype,
+    'parents' => [{'id' => config['main']['google_drive_folder_id']}]
+  })
+
+  media = Google::APIClient::UploadIO.new(filepath, mimetype)
+  result = client.execute(
+    :api_method => drive.files.insert,
+    :body_object => file,
+    :media => media,
+    :parameters => {
+      'uploadType' => 'multipart',
+      'alt' => 'json'
+    }
+  )
+end
+
+
+if __FILE__ == $0
+  client, drive = setup()
+  curdir = File.dirname(__FILE__)
+  config = IniFile.load(File.join(curdir, 'config.ini'))
+
+  gdrive_file_info = get_grive_file_info(client, drive, config)
+
+  local_files = Set.new
+  Dir.glob(File.join(config['main']['local_backup_dir'], '*.db')).each do |filepath|
+    filename = File.basename(filepath)
+    if gdrive_file_info.has_key?(filename)
+      md5 = Digest::MD5.file(filepath).hexdigest
+      if md5 != gdrive_file_info[filename]['md5Checksum']
+        puts "#{filename} md5 does not match, will update"
+        update_file(client, drive, filepath, config)
+      else
+        puts "#{filename} md5 matches, no need to update"
+      end
+    else
+      puts "#{filename} does not exist on google drive, will upload"
+      insert_file(client, drive, filepath, config)
+    end
+  end
+
+  insert_file(client, drive)
+end
+
